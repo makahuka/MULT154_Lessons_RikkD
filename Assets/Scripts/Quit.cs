8be20d7f13ff1a72a727532d0947fe9538ab29f2
makahuka <61301727+makahuka@users.noreply.github.com>
2020-09-11 13:06:24 -0700

    Lesson 2
    Multiplayer: Single Player GUI Setup
    Physics and Input
    Multiplayer: Client Server Communication

---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using System.Collections.Specialized;
+//using System.Runtime.Hosting;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit(); // ApplicationActivator.Quit();  BitVector32
+        }
+    }
+}

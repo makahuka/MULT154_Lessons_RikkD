8d1237b2d90ae68bc98b63181a3055b01d79c065
makahuka <61301727+makahuka@users.noreply.github.com>
2020-12-14 19:33:50 -0800

    Unit 8 Lessons
    DontDestroyOnLoad
    State Pattern
    PlayerPrefs
    File IO

--- Assets/Scripts/PlayerContext.cs
+++ Assets/Scripts/PlayerContext.cs
@@ -22,6 +22,8 @@ public abstract class PlayerState
 
     public abstract void FixedUpdate();
 
+    public abstract void OnCollisionEnter(Collision collision);
+
     public abstract void OnTriggerEnter(Collider other);
 
     public abstract void OnTriggerExit(Collider other);
@@ -38,6 +40,7 @@ public class RiverState : PlayerState
     public RiverState(NetworkBehaviour thisObj) : base(thisObj)
     {
         stateName = "RiverLevel";
+        GameData.gamePlayStart = Time.time;
     }
 
     // Start is called before the first frame update
@@ -94,11 +97,16 @@ public class RiverState : PlayerState
         rbPlayer.velocity = Vector3.zero;
     }
 
+    public override void OnCollisionEnter(Collision collision)
+    {
+
+    }
+
     public override void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Exit"))
         {
-            NetworkManager network =
+            NetworkManager networkManager =
                 GameObject.Find("NetworkManager").GetComponent<NetworkManager>();
             networkManager.ServerChangeScene("ForestLevel");
         }
@@ -113,19 +121,144 @@ public class RiverState : PlayerState
     }
 }
 
-/*public class ForestState : PlayerState
+public class ForestState : PlayerState
 {
+    public float speed = 10.0f;
+    public float rotationSpeed = 30.0f;
+    Rigidbody rgBody = null;
+    float trans = 0;
+    float rotate = 0;
+    private Animator anim;
+    private Camera camera;
+    private Transform lookTarget;
+
+    public delegate void DropHive(Vector3 pos);
+    public static event DropHive DroppedHive;
+
     public ForestState(NetworkBehaviour thisObj) : base(thisObj)
     {
         stateName = "ForestLevel";
     }
-}*/
+
+    public override void Start()
+    {
+        player.transform.position = new Vector3(-20, .5f, -10);
+
+        Transform rabbit = player.transform.Find("Rabbit");
+        rabbit.transform.localEulerAngles = Vector3.zero;
+        rabbit.transform.localScale = Vector3.one;
+
+        rgBody = player.GetComponent<Rigidbody>();
+        anim = player.GetComponentInChildren<Animator>();
+        camera = player.GetComponentInChildren<Camera>();
+        camera.enabled = true;
+        lookTarget = GameObject.Find("HeadAimTarget").transform;
+    }
+
+    public override void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) // something like this in MovePawn script
+        {
+            DroppedHive?.Invoke(player.transform.position + (player.transform.forward * 10));
+        }
+        // Get the horizontal and vertical axis.
+        // By default they are mapped to the arrow keys.
+        // The value is in the range -1 to 1
+        float translation = Input.GetAxis("Vertical");
+        float rotation = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("speed", translation);
+
+        trans += translation;
+        rotate += rotation;
+    }
+
+    public override void FixedUpdate()
+    {
+        Vector3 rot = player.transform.rotation.eulerAngles;
+        rot.y += rotate * rotationSpeed * Time.deltaTime;
+        rgBody.MoveRotation(Quaternion.Euler(rot));
+        rotate = 0;
+
+        Vector3 move = player.transform.forward * trans * speed;
+        move.y = rgBody.velocity.y;
+        rgBody.velocity = move; // * Time.deltaTime; 
+        trans = 0;
+    }
+
+    public override void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Hazard"))
+        {
+            anim.SetTrigger("died");
+            thisObject.StartCoroutine(ZoomOut());
+        }
+        else
+        {
+            anim.SetTrigger("twitchLeftEar");
+        }
+    }
+
+    IEnumerator ZoomOut()
+    {
+        const int ITERATIONS = 25;
+        for (int z = 0; z < ITERATIONS; z++)
+        {
+            camera.transform.Translate(camera.transform.forward * -1 * 15.0f / ITERATIONS);
+            yield return new WaitForSeconds(1.0f / ITERATIONS);
+        }
+    }
+
+    public override void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hazard"))
+        {
+            //lookTarget.position = other.transform.position;
+            thisObject.StartCoroutine(LookAndLookAway(lookTarget.position, other.transform.position));
+        }
+
+        if (other.CompareTag("Exit"))
+        {
+            NetworkManager networkManager =
+                GameObject.Find("NetworkManager").GetComponent<NetworkManager>();
+            networkManager.ServerChangeScene("EndScene");
+        }
+    }
+
+    public override void OnTriggerExit(Collider other)
+    {
+
+    }
+
+    private IEnumerator LookAndLookAway(Vector3 targetPos, Vector3 hazardPos)
+    {
+        Vector3 targetDir = targetPos - player.transform.position;
+        Vector3 hazardDir = hazardPos - player.transform.position;
+
+        float angle = Vector2.SignedAngle(new Vector2(targetPos.x, targetPos.z), new Vector2(hazardPos.x, hazardPos.z));
+
+        const int INTERVALS = 20;
+        const float INTERVAL = 0.5f / INTERVALS;
+
+        float angleInterval = angle / INTERVALS;
+
+        for (int i = 0; i < INTERVALS; i++)
+        {
+            lookTarget.RotateAround(player.transform.position, Vector3.up, -angleInterval);
+            yield return new WaitForSeconds(INTERVAL);
+        }
+
+        for (int i = 0; i < INTERVALS; i++)
+        {
+            lookTarget.RotateAround(player.transform.position, Vector3.up, angleInterval);
+            yield return new WaitForSeconds(INTERVAL);
+        }
+    }
+}
 
 public class PlayerContext : NetworkBehaviour
 {
     PlayerState currentState;
-    RiverState riverState;
-    //ForestState forestState;
 
     // Start is called before the first frame update
     void Start()
@@ -137,7 +270,19 @@ public class PlayerContext : NetworkBehaviour
             currentState = new RiverState(this);
         }
 
-        currentState.Start();
+        else if (SceneManager.GetActiveScene().name == "ForestLevel")
+        {
+            currentState = new ForestState(this);
+        }
+        else 
+        {
+            this.gameObject.SetActive(false);
+        }
+
+        if (currentState != null)
+        {
+            currentState.Start();
+        }
     }
 
     // Update is called once per frame
@@ -153,6 +298,13 @@ public class PlayerContext : NetworkBehaviour
         if (!isLocalPlayer) return;
 
         currentState.Update();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!isLocalPlayer) return;
+
+        currentState.OnCollisionEnter(collision);
     }
 
     void OnTriggerEnter(Collider other)
---
+++ Assets/Scripts/ScoreList.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using TMPro;
+
+[System.Serializable] public class Score
+{
+    public Score(string n, float t)
+    {
+        name = n;
+        time = t;
+    }
+
+    public string name;
+    public float time;
+}
+
+public class ScoreList : MonoBehaviour
+{
+    public List<Score> scores = new List<Score>();
+    public string fileName;
+    public GameObject input;
+
+    public GameObject finalPanel;
+    public GameObject scorePanel;
+
+    public GameObject entryPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(File.Exists(fileName))
+        {
+            using (BinaryReader reader = new BinaryReader(File.Open(fileName, FileMode.Create)))
+            {
+                while (true)
+                {
+                    try
+                    {
+                        string name = reader.ReadString();
+                        float time = reader.ReadSingle();
+
+                        scores.Add(new Score(name, time));
+                    }
+                    catch (EndOfStreamException)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NewEntry()
+    {
+        // Get the name input field
+        string name = input.GetComponent<TMP_InputField>().text;
+        // Get GameData component
+        // Time get current time and sub game play start
+        float time = Time.time - GameData.gamePlayStart;
+
+        scores.Insert(0, new Score(name, time));
+
+        int offset = 50;
+
+        foreach(Score score in scores)
+        {
+            GameObject temp = Instantiate(entryPrefab);
+            Transform[] children = temp.GetComponentsInChildren<Transform>();
+            children[1].GetComponent<TextMeshProUGUI>().text = score.name;
+            children[2].GetComponent<TextMeshProUGUI>().text = score.time.ToString("F2");
+
+            temp.transform.SetParent(scorePanel.transform);
+            RectTransform rtrans = temp.GetComponent<RectTransform>();
+            rtrans.anchorMin = new Vector2(.5f, .5f);
+            rtrans.anchorMax = new Vector2(.5f, .5f);
+            rtrans.pivot = new Vector2(.5f, .5f);
+            rtrans.localPosition = new Vector3(0, offset, 0);
+
+            offset -= 35;
+
+        }
+
+        finalPanel.SetActive(false);
+        scorePanel.SetActive(true);
+    }
+
+    private void OnDestroy()
+    {
+        using(BinaryWriter write = new BinaryWriter(File.Open(fileName, FileMode.OpenOrCreate)))
+        {
+            foreach(Score score in scores)
+            {
+                write.Write(score.name);
+                write.Write(score.time);
+            }
+        }
+    }
+}
